5409a24d754fe3b625180b443c86727af23b181a
klimkomania <klimkomania@interia.pl>
2017-04-07 12:30:02 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var triangle1Area = getTriangleArea(10,15),
+    triangle2Area = getTriangleArea(20,30),
+    triangle3Area = getTriangleArea(30,50);
+
+function getTriangleArea(a, h) {
+    var result = a*h/2;
+    if ((a<=0)||(h<=0)) {
+      console.log ("Niewłaściwe dane"); }
+      else {
+        console.log (result); }
+}
