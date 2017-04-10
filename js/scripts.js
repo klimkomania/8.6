cbc946b526a7d137ebdd8c6b82beea0c110c9bc9
klimkomania <klimkomania@interia.pl>
2017-04-10 07:43:29 +0200

    remove var result and return

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-var triangle1Area = getTriangleArea(10,15),
-    triangle2Area = getTriangleArea(20,30),
-    triangle3Area = getTriangleArea(30,50);
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(20, 30),
+	triangle3Area = getTriangleArea(30, 50);
 
 function getTriangleArea(a, h) {
-    var result = a*h/2;
-    if ((a<=0)||(h<=0)) {
-      console.log ("Niewłaściwe dane"); }
-      else {
-        return result; }
+	if ((a<=0)||(h<=0)) {
+    	console.log ("Niewłaściwe dane"); 
+    	} else {
+        		return a*h/2; 
+        }
 }
+
+console.log(triangle1Area, triangle2Area, triangle3Area);
